7b308be9fae9fc58086f94a8b45511a6fe39aa8c
강병욱 <98394884+plain1@users.noreply.github.com>
2022-12-30 19:47:05 +0900

    [Silver IV] Title: 큐 2, Time: 1732 ms, Memory: 175124 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.py
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -1,30 +1,30 @@
 import sys
 from collections import deque
-q=deque([])
-N=int(sys.stdin.readline())
+q = deque([])
+N= int(sys.stdin.readline())
 for i in range(N):
-    order = sys.stdin.readline().split()
-    if order[0] == 'push':
-        q.append(order[1])
-    elif order[0] == 'pop':
-        if len(q) != 0 :
-            print(q.popleft())
-        else:
+    cmd = sys.stdin.readline().split() 
+    if cmd[0] == 'push':
+            q.append(cmd[1])
+    elif cmd[0] == 'pop':
+        if len(q) == 0:
             print(-1)
-    elif order[0] == 'size':
-        print(len(q))
-    elif order[0] == 'empty':
+        else:
+            print(q.popleft())
+    elif cmd[0] == 'size':
+            print(len(q))
+    elif cmd[0] == 'empty':
         if len(q) == 0:
             print(1)
         else:
             print(0)
-    elif order[0] == 'front':
-        if len(q) != 0:
+    elif cmd[0] == 'front':
+        if len(q) == 0:
+            print(-1)
+        else:
             print(q[0])
-        else:
+    elif cmd[0] == 'back':
+        if len(q) == 0:
             print(-1)
-    elif order[0] == 'back':
-        if len(q) != 0:
+        else:
             print(q[-1])
-        else:
-            print(-1)

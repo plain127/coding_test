a7e5c22c306e22a23ffaf88bc62a2dbfba49c69a
강병욱 <98394884+plain1@users.noreply.github.com>
2023-01-09 16:26:04 +0900

    [Bronze II] Title: 수 정렬하기, Time: 64 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -1,13 +1,20 @@
-N=int(input())
-if N >= 1 and N <= 1000:
-    n = list(0 for i in range(0,N))
-    for i in range(N):
-        n[i] = int(input())
-    for i in range(1,N):
-        key = n[i]
-        while(i > 0 and n[i-1]>n[i]):
-            n[i] = n[i-1]
-            n[i-1] = key
-            i-=1
+import sys
+N = int(sys.stdin.readline())
+arr = []
 for i in range(N):
-    print(n[i])
+    n = int(sys.stdin.readline())
+    arr.append(n)
+
+def selection_sort(A):
+    n=len(A)
+    for i in range(n-1):
+        least = i
+        for j in range(i+1,n):
+            if(A[j]<A[least]):
+                least = j
+        A[i],A[least] = A[least],A[i]
+
+selection_sort(arr)
+
+for i in arr:
+    print(i)

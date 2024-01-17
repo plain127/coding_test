e7f930b274e477dd5b54e451f57f8197c25dca0f
Plain ByeongUk Kang <plain_k1207@naver.com>
2024-01-17 23:09:20 +0900

    knight of royal

--- 이것이 취업을 위한 코딩테스트다 with 파이썬 책 문제 풀기/구현(완전탐색, 시뮬레이션)/왕실의 나이트.py
+++ 이것이 취업을 위한 코딩테스트다 with 파이썬 책 문제 풀기/구현(완전탐색, 시뮬레이션)/왕실의 나이트.py
@@ -1,5 +1,18 @@
 #나의 풀이
+
+#책 풀이
 pos = input()
 row = int(pos[1])
-col = pos[0]
-s
+col = int(ord(pos[0])) - int(ord('a')) + 1
+
+steps = [(-2,-1),(-1,-2),(1,-2),(2,-1),(2,1),(1,2),(-1,2),(-2,1)]
+count = 0
+
+for step in steps:
+    next_row = row + step[0]
+    next_col = col + step[1]
+
+    if next_row >=1 and next_row <= 8 and next_col >=1 and next_col <=8:
+        count += 1
+        
+print(count)

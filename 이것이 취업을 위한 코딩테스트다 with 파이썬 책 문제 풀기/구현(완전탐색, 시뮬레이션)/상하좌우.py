8763b95b8217c7cc004f65317d448cec7a7bccf1
Plain ByeongUk Kang <plain_k1207@naver.com>
2024-07-12 22:02:31 +0900

    implement sol

--- 이것이 취업을 위한 코딩테스트다 with 파이썬 책 문제 풀기/구현(완전탐색, 시뮬레이션)/상하좌우.py
+++ 이것이 취업을 위한 코딩테스트다 with 파이썬 책 문제 풀기/구현(완전탐색, 시뮬레이션)/상하좌우.py
@@ -1,39 +1,42 @@
-#나의 풀이법
+#내가 푼 풀이
 n = int(input())
-x, y = 1, 1
-plans = list(input().split())
+direction = list(map(str,input().split()))
 
-for i in plans:
-    if i == 'R':
-        if y<n:
-            y+=1
-    elif i== 'L':
-        if y>1:
-            y-=1
-    elif i == 'U':
-        if x>1:
-            x-=1
-    elif i == 'D':
-        if x<n:
-            x+=1
+x = 1
+y = 1
 
-print(x, y)
+for dir in direction:
+    if dir == 'L':
+        if x > 1:
+            x -= 1
+    elif dir == 'R':
+        if x < n:
+            x += 1
+    elif dir == 'U':
+        if y > 1:
+            y -= 1
+    elif dir == 'D':
+        if y < n:
+            y += 1
 
-#책 풀이법
+print(f'{y} {x}')
+
+#책 풀이
 n = int(input())
 x, y = 1, 1
 plans = input().split()
 
-dx = [0,0,-1,1]
-dy = [-1,1,0,0]
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
 move_types = ['L', 'R', 'U', 'D']
 
 for plan in plans:
     for i in range(len(move_types)):
         if plan == move_types[i]:
             nx = x + dx[i]
-            ny = y + dy[i]
-    if nx < 1 or nx > n or ny < 1 or ny > n:
+            ny = x + dy[i]
+
+    if nx < 1 or ny < 1 or nx > n or ny > n:
         continue
     x, y = nx, ny
 

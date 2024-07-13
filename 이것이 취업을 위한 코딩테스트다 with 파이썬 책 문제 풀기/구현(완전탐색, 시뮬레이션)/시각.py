96c25472d51e94fd6b76e2ca862db60b74f30ad5
Plain ByeongUk Kang <plain_k1207@naver.com>
2024-07-13 19:25:32 +0900

    bruteforce sol

--- 이것이 취업을 위한 코딩테스트다 with 파이썬 책 문제 풀기/구현(완전탐색, 시뮬레이션)/시각.py
+++ 이것이 취업을 위한 코딩테스트다 with 파이썬 책 문제 풀기/구현(완전탐색, 시뮬레이션)/시각.py
@@ -1,16 +1,35 @@
-#나의 풀이
-
-#책 풀이
+#내가 푼 풀이
 n = int(input())
+
+time = n*3600
+hour = 0
+min = 0
+sec = 0
 
 count = 0
 
-for h in range(n+1):
-    for m in range(60):
-        for s in range(60):
-            if '3' in str(h) + str(m) + str(s):
-                count+=1
+while (hour*3600) <= time:
+    sec += 1
+    if sec == 60 :
+        min += 1
+        sec = 0
+    if min == 60 :
+        hour += 1
+        min = 0
+    if '3' in str(hour) + str(min) + str(sec):
+        count += 1
 
 print(count)
 
+# 책 풀이
 
+h = int(input())
+
+count = 0
+for i in range(h+1):
+    for j in range(60):
+        for k in range(60):
+            if '3' in str(i) + str(j) + str(k):
+                count += 1
+
+print(count)

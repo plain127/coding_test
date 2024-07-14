b4129206af8d8a9b2251d64789cb1f76751a1493
Plain ByeongUk Kang <plain_k1207@naver.com>
2024-07-14 18:29:26 +0900

    not sol

--- 이것이 취업을 위한 코딩테스트다 with 파이썬 책 문제 풀기/구현(완전탐색, 시뮬레이션)/왕실의 나이트.py
+++ 이것이 취업을 위한 코딩테스트다 with 파이썬 책 문제 풀기/구현(완전탐색, 시뮬레이션)/왕실의 나이트.py
@@ -1,18 +1,27 @@
-#나의 풀이
+#내가 푼 풀이
+loc = input()
+
+loc_x = int(ord(loc[0])) - 96
+loc_y = loc[1]
+
+dx = [-2,-1,1,2]
+dy = [-2,-1,1,2]
+
+count = 0
+#gg
 
 #책 풀이
-pos = input()
-row = int(pos[1])
-col = int(ord(pos[0])) - int(ord('a')) + 1
+input_data = input()
+row = int(input_data[1])
+column = int(ord(input_data[0])) - int(ord('a')) + 1
 
 steps = [(-2,-1),(-1,-2),(1,-2),(2,-1),(2,1),(1,2),(-1,2),(-2,1)]
-count = 0
 
+result = 0
 for step in steps:
     next_row = row + step[0]
-    next_col = col + step[1]
+    next_column = column + step[1]
 
-    if next_row >=1 and next_row <= 8 and next_col >=1 and next_col <=8:
-        count += 1
-        
-print(count)
+    if next_row >= 1 and next_row <=8 and next_column >= 1 and next_column <= 8:
+        result +=1
+print(result)

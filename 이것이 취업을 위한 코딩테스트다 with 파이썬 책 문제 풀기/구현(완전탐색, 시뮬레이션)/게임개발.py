59483a4e17c738deb32a30dced1d1f038075314e
Plain ByeongUk Kang <plain_k1207@naver.com>
2024-07-15 23:43:28 +0900

    게임개발 sol fail

--- 이것이 취업을 위한 코딩테스트다 with 파이썬 책 문제 풀기/구현(완전탐색, 시뮬레이션)/게임개발.py
+++ 이것이 취업을 위한 코딩테스트다 with 파이썬 책 문제 풀기/구현(완전탐색, 시뮬레이션)/게임개발.py
@@ -1,44 +1,48 @@
 #내가 푼 풀이
+import sys
 n, m = map(int, input().split())
 
-col, row, user_dir = map(int, input().split())
-
-dir = [0,1,2,3]
-around = []
-
-for i in range(n):
-    around.append(list(map(int,input().split())))
-
-count = 0
-
-
-#책 풀이
-n, m = map(int, input().split())
-
-d =[[0]*m for _ in range(n)]
+d = [0,1,2,3]
+map = []
 
 x, y, dir = map(int, input().split())
+
+for i in range(n):
+    for j in range(m):
+        k = sys.stdin.readline.split()
+        map.append(k)
+
+if dir == d[0]:
+    dir = d[-1]
+    x-=1
+
+#책 풀이
+n, m = map(int,input().split())
+
+d = [[0]*m for _ in range(n)]
+
+x, y, direction = map(int, input().split())
 d[x][y] = 1
 
 array = []
 for i in range(n):
     array.append(list(map(int,input().split())))
 
-dx = [-1,0,1,0]
-dy = [0,1,0,-1]
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
 
 def turn_left():
-    global dir
-    dir -= 1
-    if dir == -1:
-        dir = 3
+    global direction
+    direction -= 1
+    if direction == -1:
+        direction = 3
 
 count = 1
 turn_time = 0
 while True:
     turn_left()
-    nx = x + dx[dir]
-    ny = y + dy[dir]
+    nx = x + dx[direction]
+    ny = y + dy[direction]
 
     if d[nx][ny] == 0 and array[nx][ny] == 0:
         d[nx][ny] = 1
@@ -50,8 +54,8 @@ while True:
     else:
         turn_time += 1
     if turn_time == 4:
-        nx = x - dx[dir]
-        ny = y - dy[dir]
+        nx = x - dx[direction]
+        ny = y - dy[direction]
 
         if array[nx][ny] == 0:
             x = nx

362617d7359b5ee2897d77fcebcf9b54723788a0
Plain ByeongUk Kang <plain_k1207@naver.com>
2024-07-17 23:22:40 +0900

    bfs cant sol

--- 이것이 취업을 위한 코딩테스트다 with 파이썬 책 문제 풀기/DFS,BFS/미로탈출.py
+++ 이것이 취업을 위한 코딩테스트다 with 파이썬 책 문제 풀기/DFS,BFS/미로탈출.py
@@ -1,14 +1,32 @@
-#내가 푼 풀이
+# 내가 푼 풀이
+from collections import deque
+
+n, m = map(int, input().split())
+
+coord = []
+
+for i in range(n):
+    coord.append(list(map(int, input())))
+
+count = 0
+
+def bfs(x,y):
+    
+
+for x in range(n):
+    for y in range(m):
+        bfs(x,y)
+
+print(count)
 
 
-#책 풀이
+# 책 풀이
 from collections import deque
 
 n, m = map(int,input().split())
-
 graph = []
 for i in range(n):
-    graph.append(list(map(int,input())))
+    graph.append(list(map(int, input())))
 
 dx = [-1,1,0,0]
 dy = [0,0,-1,1]
@@ -16,21 +34,21 @@ dy = [0,0,-1,1]
 def bfs(x, y):
     queue = deque()
     queue.append((x,y))
-
     while queue:
         x, y = queue.popleft()
+
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
 
-            if nx < 0 or ny < 0 or nx >=n or ny >=m:
+            if nx < 0 or ny < 0 or nx >= n or ny >= m:
                 continue
             if graph[nx][ny] == 0:
                 continue
             if graph[nx][ny] == 1:
                 graph[nx][ny] = graph[x][y] + 1
-                queue.append((nx,ny))
+                queue.append((nx, ny))
 
-    return graph[n-1][m-1] 
+    return graph[n - 1][m - 1]
 
-print(bfs(0,0))
+print(bfs(0, 0))

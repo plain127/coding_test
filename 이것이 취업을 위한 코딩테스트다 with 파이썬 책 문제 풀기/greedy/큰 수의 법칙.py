00c122c46243580319a47be0a1e11641445a9e4f
plain127 <plain_k1207@naver.com>
2024-08-19 18:33:06 +0900

    solved greedy

--- 이것이 취업을 위한 코딩테스트다 with 파이썬 책 문제 풀기/greedy/큰 수의 법칙.py
+++ 이것이 취업을 위한 코딩테스트다 with 파이썬 책 문제 풀기/greedy/큰 수의 법칙.py
@@ -1,54 +1,34 @@
 #내가 푼 풀이
 n, m, k = map(int, input().split())
 
-arr = list(map(int, input().split()))
-arr.sort(reverse=True)
-
-result = 0
-for i in range(1, m+1):
-    if (i%(k+1) == 0):
-        result += arr[1]
-    else:
-        result += arr[0]
-
-print(result)
-
-#책 풀이
-#단순한 풀이
-n, m, k = map(int, input().split())
-
 data = list(map(int, input().split()))
-data.sort()
-frist = data[n-1]
-second = data[n-2]
+
+data.sort(reverse=True)
 
 result = 0
 
-while True:
-    for i in range(k):
-        if m == 0:
-            break
-        result += first
-        m -= 1
-    if m == 0:
-        break
-    result += second
-    m -= 1
+for i in range(1, k + 1):
+    result += data[0]
+             
+m_num = int(m // k)
+k_num = int(m % k)
+
+result = m_num * result
+result += data[1] * k_num
 
 print(result)
 
-#반복수열의 아이디어 반영
-
+# 책 풀이
 n, m, k = map(int, input().split())
 
 data = list(map(int, input().split()))
 
 data.sort()
-first = data[n-1]
-second = data[n-2]
+first = data[n - 1]
+second = data[n - 2]
 
-count = int(m / (k+1)) * k
-count += m % (k+1)
+count = int(m / (k + 1) * k)
+count += m % (k + 1)
 
 result = 0
 result += (count) * first

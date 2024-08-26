f2add7360bbc19f8aa47d2738a9ee9497a6aa1ca
plain127 <plain_k1207@naver.com>
2024-08-26 21:19:42 +0900

    solved greedy

--- 이것이 취업을 위한 코딩테스트다 with 파이썬 책 문제 풀기/greedy/1이 될 때까지.py
+++ 이것이 취업을 위한 코딩테스트다 with 파이썬 책 문제 풀기/greedy/1이 될 때까지.py
@@ -1,51 +1,30 @@
-# 내가 푼 풀이
+# 내가 푼풀이
 n, k = map(int, input().split())
 
-count = 0
+count = n%k
+
 while True:
-    if n > 1 :
-        if n%k == 0:
-            n /= k
-        else:
-            n-=1
-        count += 1
-    elif n == 1:
+    n = n//k
+    count += 1
+    if n == 1:
         break
 
-print(count)
+print(count) 
 
 # 책 풀이
-## 단순하게 푸는 답안 
-n, k = map(int, input().split())
-result = 0
-
-while n >= k:
-    while n % k != 0:
-        n -= 1
-        result += 1
-
-    n //= k
-    result += 1
-
-while n > 1:
-    n -= 1
-    result += 1
-
-print(result)
-
-#답안 예시
 n, k = map(int, input().split())
 result = 0
 
 while True:
-    target = (n // k) * k
+    target = (n // k)*k
     result += (n - target)
     n = target
-
+    
     if n < k:
         break
+    
     result += 1
     n //= k
-
+    
 result += (n - 1)
 print(result)

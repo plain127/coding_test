49c4892b426260144a0217e6bc02676172ef60de
강병욱 <98394884+plain127@users.noreply.github.com>
2024-08-31 15:18:05 +0900

    [Silver III] Title: 주유소, Time: 96 ms, Memory: 46788 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -1,17 +1,15 @@
 n = int(input())
+
 dis = list(map(int, input().split()))
 price = list(map(int, input().split()))
 
 result = dis[0]*price[0]
 
-group = []
+min_price = price[0]
 
 for i in range(1, n-1):
-    a = [price[i], dis[i]]
-    group.append(a)
-    
-group.sort()
-
-result += group[0][0] * (sum(dis)- dis[0])
+    if price[i] < min_price:
+        min_price = price[i]
+    result += min_price * dis[i]
 
 print(result)

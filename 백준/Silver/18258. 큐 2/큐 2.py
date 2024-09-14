90dde28d94b0434bab506abd4951f1bf728ba44e
강병욱 <98394884+plain127@users.noreply.github.com>
2024-09-14 15:34:31 +0900

    [Silver IV] Title: 큐 2, Time: 1576 ms, Memory: 110568 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.py
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -1,30 +1,36 @@
 import sys
 from collections import deque
-q = deque([])
-N= int(sys.stdin.readline())
-for i in range(N):
-    cmd = sys.stdin.readline().split() 
-    if cmd[0] == 'push':
-            q.append(cmd[1])
-    elif cmd[0] == 'pop':
+
+n = int(sys.stdin.readline().strip())
+q = deque()
+results = []
+
+for _ in range(n):
+    order = sys.stdin.readline().strip().split()
+    if order[0] == 'push':
+        q.append(int(order[1]))
+    elif order[0] == 'pop':
         if len(q) == 0:
-            print(-1)
+            results.append(-1)
         else:
-            print(q.popleft())
-    elif cmd[0] == 'size':
-            print(len(q))
-    elif cmd[0] == 'empty':
+            results.append(q.popleft())
+    elif order[0] == 'size':
+        results.append(len(q))
+    elif order[0] == 'empty':
         if len(q) == 0:
-            print(1)
+            results.append(1)
         else:
-            print(0)
-    elif cmd[0] == 'front':
+            results.append(0)
+    elif order[0] == 'front':
         if len(q) == 0:
-            print(-1)
+            results.append(-1)
         else:
-            print(q[0])
-    elif cmd[0] == 'back':
+            results.append(q[0])
+    elif order[0] == 'back':
         if len(q) == 0:
-            print(-1)
+            results.append(-1)
         else:
-            print(q[-1])
+            results.append(q[-1])
+            
+for result in results:
+    print(result)

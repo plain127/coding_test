e5f6a0cd4a4f0fbce7b34b03267448fbad6aab2a
강병욱 <98394884+plain127@users.noreply.github.com>
2024-10-17 23:55:37 +0900

    [Silver IV] Title: 수 찾기, Time: 140 ms, Memory: 50540 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+a = set(map(int, input().split()))
+
+m = int(input())
+b = list(map(int, input().split()))
+
+for num in b:
+    if num in a:
+        print(1)
+    else:
+        print(0)

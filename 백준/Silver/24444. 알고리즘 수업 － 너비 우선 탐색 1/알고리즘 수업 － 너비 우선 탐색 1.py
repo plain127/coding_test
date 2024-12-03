3e5cdb34cf55d71faf290a8c3e81e3547934c554
강병욱 <98394884+plain127@users.noreply.github.com>
2024-12-03 11:10:11 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 528 ms, Memory: 58984 KB -BaekjoonHub

--- 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.py
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.py
@@ -2,39 +2,35 @@ import sys
 from collections import deque
 
 input = sys.stdin.readline
-#in_file = open('input.txt', 'r')
-
-def bfs(graph, start, visited):
-    global count
-    queue = deque([start])
-
-    visited[start] = count
-    count+=1
-
-    while queue:
-        v = queue.popleft()
-
-        for i in graph[v]:
-            if visited[i] == 0:
-                queue.append(i)
-                visited[i] = count
-                count+=1
 
 n, m, r = map(int, input().split())
-graph = [[] for _ in range(n+1)]
-visited = [0]*(n+1)
 
+graph = [[] for _ in range(n+1)]
 for _ in range(m):
     u, v = map(int, input().split())
     graph[u].append(v)
     graph[v].append(u)
-
-for i in range(n+1):
+for i in range(1, n+1):
     graph[i].sort()
 
+visited = [0]*(n+1)
 count = 1
 
-bfs(graph, r, visited)
+def bfs(r):
+    q = deque([r])
+    global count
+    
+    visited[r] += count
+    count += 1
+    
+    while q:
+        v = q.popleft()
+        for i in graph[v]:
+            if visited[i] == 0:
+                q.append(i)
+                visited[i] += count
+                count += 1
 
+bfs(r)
 for i in range(1,n+1):
     print(visited[i])

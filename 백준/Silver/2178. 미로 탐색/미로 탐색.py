23ec35cdcd38ad5b600cbd630381250b33fe8695
강병욱 <98394884+plain127@users.noreply.github.com>
2024-12-22 11:38:02 +0900

    [Silver I] Title: 미로 탐색, Time: 68 ms, Memory: 34952 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,36 +1,29 @@
 import sys
 from collections import deque
-
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-graph = []
-
-for _ in range(n):
-    graph.append(list(map(int, input().strip())))
-
+graph = [list(map(int, input().strip())) for _ in range(n)]
 dx = [-1,1,0,0]
 dy = [0,0,-1,1]
 
 def bfs(x, y):
-    q = deque([(x, y, 1)])
+    q = deque()
+    q.append([x, y, 1])
     graph[y][x] = 0
     
-    while q:
+    while q :
         x, y, count = q.popleft()
-        
-        if x == m -1 and y == n - 1:
+
+        if x == m-1 and y == n-1:
             return count
-        
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            
-            if nx >= 0 and nx < m and ny >=0 and ny < n and graph[ny][nx] == 1:
+            if 0<=nx<m and 0<=ny<n and graph[ny][nx] == 1:
                 graph[ny][nx] = 0
-                q.append((nx, ny, count + 1))
-    
+                q.append([nx, ny, count+1])
 
-result = bfs(0, 0)
+result = bfs(0,0)
 
-print(result)  
+print(result)

257ea56df401223762ebe14ad64fbd3a96c1496b
Plain ByeongUk Kang <plain_k1207@naver.com>
2024-12-24 13:37:46 +0900

    bfs wrong answer

---
+++ 이것이 취업을 위한 코딩테스트다 with 파이썬 책 문제 풀기/DFS,BFS/블록이동하기.py
@@ -0,0 +1,83 @@
+#내풀이
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+board = [[0,0,0,1,1],[0,0,0,1,0],[0,1,0,1,1],[1,1,0,0,1],[0,0,0,0,0]]
+
+def solution(board):
+    dx = [-1,1,0,0]
+    dy = [0,0,-1,1]
+
+    time = 0
+
+    q = deque()
+    q.append((0,0,0,1))
+    board[0][0] = -1
+    board[0][1] = -1
+
+    while q:
+        x1, y1, x2, y2 = q.popleft()
+        for i in range(4):
+            nx1 = x1 + dx[i]
+            ny1 = y1 + dy[i]
+
+def rotate(x, y, board):
+    rx = [-1,-1,-1,0,0,1,1,1]
+    ry = [-1,0,1,-1,1,-1,0,1]
+
+    for i in range(8):
+        nx = x + rx[i]
+        ny = y + ry[i]
+
+#책 풀이
+from collections import deque
+
+def get_next_pos(pos, board):
+    next_pos = []
+    pos = list(pos)
+    pos1_x, pos1_y, pos2_x, pos2_y = pos[0][0], pos[0][1], pos[1][0], pos[1][1]
+
+    dx = [-1,1,0,0]
+    dy = [0,0,-1,1]
+    for i in range(4):
+        pos1_next_x, pos1_next_y, pos2_next_x, pos2_next_y = pos1_x + dx[i], pos1_y + dy[i], pos2_x + dx[i], pos2_y + dy[i]
+
+        if board[pos1_next_x][pos1_next_y] == 0 and board[pos2_next_x][pos2_next_y] == 0:
+            next_pos.append({(pos1_next_x, pos1_next_y), (pos2_next_x, pos2_next_y)})
+
+    if pos1_x == pos2_x:
+        for i in [-1,1]:
+            if board[pos1_x+i][pos1_y] == 0 and board[pos2_x + i][pos2_y] == 0:
+                next_pos.append({(pos1_x, pos1_y), (pos1_x + i, pos1_y)})
+                next_pos.append({(pos2_x, pos2_y), (pos2_x + i, pos2_y)})
+    elif pos1_y == pos2_y:
+        for i in [-1,1]:
+            if board[pos1_x][pos1_y + i] == 0 and board[pos2_x][pos2_y + i] == 0:
+                next_pos.append({(pos1_x, pos1_y), (pos1_x, pos1_y + i)})
+                next_pos.append({(pos2_x, pos2_y), (pos2_x, pos2_y + i)})
+    
+    return next_pos
+
+def solution(board):
+    n = len(board)
+    new_board = [[1]*(n+2) for _ in range(n+2)]
+    for i in range(n):
+        for j in range(n):
+            new_board[i+1][j+1] = board[i][j]
+    q = deque()
+    visited = []
+    pos = {(1,1), (1,2)}
+    q.append((pos, 0))
+
+    while q:
+        pos, cost = q.popleft()
+        if (n, n) in pos:
+            return cost
+        
+        for next_pos in get_next_pos(pos, new_board):
+            if next_pos not in visited:
+                q.append((next_pos, cost + 1))
+                visited.append(next_pos)
+
+    return 0

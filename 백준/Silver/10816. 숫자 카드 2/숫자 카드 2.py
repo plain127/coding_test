db8445ece92d4c683c34233d430ece47486cf10b
강병욱 <98394884+plain127@users.noreply.github.com>
2024-12-31 11:07:37 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 2696 ms, Memory: 119616 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -17,16 +17,17 @@ for card in cards:
         results[card] = 1
 
 def binary_search(cards, target, start, end):
-    if start > end:
-        return 0
+    while start <= end:
+        mid = (start + end) // 2
+
+        if cards[mid] == target:
+            return results.get(target)
+        elif cards[mid] > target:
+            end = mid - 1
+        else:
+            start = mid + 1
     
-    mid = (start + end) // 2
-    if cards[mid] == target:
-        return results.get(target)
-    elif cards[mid] < target:
-        return binary_search(cards, target, mid+1, end)
-    else:
-        return binary_search(cards, target, start, mid-1)
-    
+    return 0
+
 for target in targets:
     print(binary_search(cards, target, 0, len(cards)-1), end = ' ')

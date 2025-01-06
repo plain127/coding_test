5ec609afc076d742c17555192a1028e694b439f3
Plain ByeongUk Kang <plain_k1207@naver.com>
2025-01-06 17:05:55 +0900

    wrong answer binary search

---
+++ 이것이 취업을 위한 코딩테스트다 with 파이썬 책 문제 풀기/Search/가사검색.py
@@ -0,0 +1,46 @@
+#내 풀이
+import sys
+
+input = sys.stdin.readline
+
+def solution(words, queries):
+    results = []
+    
+    for query in queries:
+        for word in words:
+            print()
+
+def binary_search(start, end, target, array):
+    while start <= end:
+        mid = (start + end) // 2
+
+#책 풀이
+from bisect import bisect_left, bisect_right
+
+def count_by_range(a, left_value, right_value):
+    right_index = bisect_right(a, right_value)
+    left_index = bisect_left(a, left_value)
+
+    return right_index - left_index
+
+array = [[] for _ in range(10001)]
+reversed_array = [[] for _ in range(10001)]
+
+def solution(words, queries):
+    answer = []
+    for word in words:
+        array[len(word)].append(word)
+        reversed_array[len(word)].append(word[::-1])
+
+    for i in range(10001):
+        array[i].sort()
+        reversed_array[i].sort()
+
+    for q in queries:
+        if q[0] != '?':
+            res = count_by_range(array[len(q)], q.replace('?', 'a'), q.replace('?', 'z'))
+        else:
+            res = count_by_range(reversed_array[len(q)], q[::-1].replace('?', 'a'), q[::-1].replace('?', 'z'))
+        answer.append(res)
+    
+    return answer

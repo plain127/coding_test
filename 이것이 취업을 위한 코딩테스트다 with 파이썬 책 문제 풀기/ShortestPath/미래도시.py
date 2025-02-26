1d0b078eaa390922451904938f81482328eff872
Plain ByeongUk Kang <plain_k1207@naver.com>
2025-02-26 13:29:59 +0900

    shortest path

--- 이것이 취업을 위한 코딩테스트다 with 파이썬 책 문제 풀기/ShortestPath/미래도시.py
+++ 이것이 취업을 위한 코딩테스트다 with 파이썬 책 문제 풀기/ShortestPath/미래도시.py
@@ -1,39 +1,63 @@
-#내가 푼 풀이
-import heapq
+#내 풀이
 import sys
 
+input = sys.stdin.readline
 INF = int(1e9)
 
 n, m = map(int, input().split())
 
-# 책 풀이
+graph = [[INF]*(n+1) for _ in range(n+1)]
+
+for a in range(1, n+1):
+    for b in range(1, n+1):
+        graph[a][b] = 0
+
+for _ in range(m):
+    x, y = map(int, input().split())
+    graph[x][y] = min(1, graph[x][y])
+    graph[y][x] = min(1, graph[y][x])
+
+x, k = map(int, input().split())
+
+for k in range(1, n+1):
+    for a in range(1, n+1):
+        for b in range(1, n+1):
+            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+        
+
+time = graph[1][k] + graph[k][x]
+
+if time >= 1e9:
+    print(-1)
+else:
+    print(time)
+
+#책 풀이
 INF = int(1e9)
 
 n, m = map(int, input().split())
+graph = [[INF]*(n+1) for _ in range(n+1)]
 
-graph = [[INF] * (n + 1) for _ in range(n + 1)]
-
-for a in range(1, n + 1):
-    for b in range(1, n + 1):
+for a in range(1,n+1):
+    for b in range(1,n+1):
         if a == b:
             graph[a][b] = 0
-            
+
 for _ in range(m):
     a, b = map(int, input().split())
     graph[a][b] = 1
     graph[b][a] = 1
-    
-x, k = map(int, input().split())
 
-for k in range(1, n + 1):
-    for a in range(1, n + 1):
-        for b in range(1, n + 1):
+x, k = map(int,input().split())
+
+for k in range(1, n+1):
+    for a in range(1, n+1):
+        for b in range(1, n+1):
             graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
-            
+
 distance = graph[1][k] + graph[k][x]
 
 if distance >= INF:
-    print("-1")
+    print(-1)
 else:
     print(distance)
-    
--- 이것이 취업을 위한 코딩테스트다 with 파이썬 책 문제 풀기/ShortestPath/전보.py
+++ 이것이 취업을 위한 코딩테스트다 with 파이썬 책 문제 풀기/ShortestPath/전보.py
@@ -1,40 +1,13 @@
-#내가 푼 풀이
-n, m, c = map(int, input().split())
-
-INF = int(1e9)
-
-graph = [[INF] * (n + 1) for _ in range(n + 1)]
-
-for a in range(1, n + 1):
-    for b in range(1, n + 1):
-        if a == b:
-            graph[a][b] = 0
-
-for _ in range(m):
-    x, y, z = map(int, input().split())
-    graph[x][y] = z
-
-for k in range(1, n + 1):
-    for x in range(1, n + 1):
-        for y in range(1, n + 1):
-            graph[x][y] = min(graph[x][y], graph[x][k] + graph[k][y])
-
-for x in range(1, n + 1):
-    for y in range(1, n + 1):
-        graph[x][y]
-
-# 책 풀이
-import heapq
+#내 풀이
 import sys
+import heapq
 
 input = sys.stdin.readline
 INF = int(1e9)
 
-n, m, c = map(int, input().split)
-
-graph = [[] for i in range(n + 1)]
-
-distance = [INF] * (n + 1)
+n, m, c = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+distance = [INF]*(n+1)
 
 for _ in range(m):
     x, y, z = map(int, input().split())
@@ -44,18 +17,64 @@ def dijkstra(start):
     q = []
     heapq.heappush(q, (0, start))
     distance[start] = 0
+
+    while q:
+        dist, now = heapq.heappop(q)
+
+        if distance[now] < dist:
+            continue
+
+        for i in graph[now]:
+            cost = dist + i[1]
+
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0])) 
+
+
+dijkstra(c)
+count = 0
+time = 0
+for i in range(n+1):
+    if distance[i] != INF:
+        count += 1
+        time += distance[i]
+
+print(count, time)
+
+#책 풀이
+import sys
+import heapq
+
+input = sys.stdin.readline
+INF = int(1e9)
+
+n, m, start = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+distance = [INF]*(n+1)
+
+for _ in range(m):
+    x, y, z = map(int, input().split())
+    graph[x].append((y, z))
+
+def dijkstra(start):
+    q = []
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+
     while q:
         dist, now = heapq.heappop(q)
         if distance[now] < dist:
             continue
+
         for i in graph[now]:
             cost = dist + i[1]
 
-        if cost < distance[i[0]]:
-            distance[i[0]] = cost
-            heapq.heappush(q, (cost, i[0]))
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
 
-dijkstra(c)
+dijkstra(start)
 
 count = 0
 max_distance = 0
@@ -65,4 +84,4 @@ for d in distance:
         count += 1
         max_distance = max(max_distance, d)
 
-print(count - 1, max_distance)
+print(count-1, max_distance)

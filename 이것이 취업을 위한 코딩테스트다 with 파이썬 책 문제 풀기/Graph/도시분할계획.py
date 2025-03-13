f9eaa11b1c2eb33c20da89145c28375f17b64da9
Plain ByeongUk Kang <plain_k1207@naver.com>
2025-03-14 02:05:12 +0900

    solved kruskal

--- 이것이 취업을 위한 코딩테스트다 with 파이썬 책 문제 풀기/Graph/도시분할계획.py
+++ 이것이 취업을 위한 코딩테스트다 with 파이썬 책 문제 풀기/Graph/도시분할계획.py
@@ -1,41 +1,53 @@
-# 내 풀이
+#내 풀이
+import sys
+
+input = sys.stdin.readline
+
 def find_parent(parent, x):
     if parent[x] != x:
         parent[x] = find_parent(parent, parent[x])
-    
     return parent[x]
 
 def union_parent(parent, a, b):
     a = find_parent(parent, a)
     b = find_parent(parent, b)
-    
     if a < b:
         parent[b] = a
     else:
         parent[a] = b
 
 n, m = map(int, input().split())
-parent = [0] * (n +1)
+parent = [0]*(n+1)
 
 edges = []
 result = 0
+sep = []
 
-for i in range(1, n + 1):
+for i in range(1, n+1):
     parent[i] = i
 
 for _ in range(m):
     a, b, c = map(int, input().split())
-    edges.append((a, b, c))
-    
+    edges.append((c, a, b))
+
+edges.sort()
+
 for edge in edges:
-    a, b, c = edge
+    c, a, b = edge
+
     if find_parent(parent, a) != find_parent(parent, b):
         union_parent(parent, a, b)
         result += c
-        
+        sep.append((c,a,b))
+
+sep.sort(reverse=True)
+sep_cost, _, _ = sep[0]
+
+result-=sep_cost
+
 print(result)
 
-# 책 풀이
+#책 풀이
 def find_parent(parent, x):
     if parent[x] != x:
         parent[x] = find_parent(parent, parent[x])
@@ -44,19 +56,19 @@ def find_parent(parent, x):
 def union_parent(parent, a, b):
     a = find_parent(parent, a)
     b = find_parent(parent, b)
-    
+
     if a < b:
         parent[b] = a
     else:
         parent[a] = b
-        
+
 v, e = map(int, input().split())
-parent = [0] * (v + 1)
+parent = [0] * (n+1)
 
 edges = []
 result = 0
 
-for i in range(1, v + 1):
+for i in range(1, v+1):
     parent[i] = i
 
 for _ in range(e):

839706bb4daf2bed7227ea6968e88b72d0534172
강병욱 <98394884+plain127@users.noreply.github.com>
2025-03-14 15:30:38 +0900

    [Silver II] Title: 유기농 배추, Time: 64 ms, Memory: 34976 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,42 +1,44 @@
 import sys
 from collections import deque
+
 input = sys.stdin.readline
 
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
+dx = [-1,0,0,1]
+dy = [0,-1,1,0]
+total = []
 
 def bfs(x, y):
-    q = deque()
+    q = deque([])
     q.append((x, y))
+    graph[y][x] = 0
 
     while q:
         x, y = q.popleft()
+
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
+            
             if 0 <= nx < m and 0 <= ny < n and graph[ny][nx] == 1:
-                graph[ny][nx] = 0
                 q.append((nx, ny))
+                graph[ny][nx] = 0
 
-
-t = int(input())
-result = []
-
-for _ in range(t):
+for _ in range(int(input())):
     m, n, k = map(int, input().split())
     graph = [[0]*m for _ in range(n)]
+
+    count = 0
     for _ in range(k):
         x, y = map(int, input().split())
         graph[y][x] = 1
 
-    count = 0
-    for row in range(n):
-        for col in range(m):
-            if graph[row][col] == 1:
-                bfs(col, row)
+    for y in range(n):
+        for x in range(m):
+            if graph[y][x] == 1:
+                bfs(x, y)
                 count += 1
     
-    result.append(count)
+    total.append(count)
 
-for i in result:
+for i in total:
     print(i)

49d8347ad66d05a98bef4db0cfe2cc3ca0a0a0ab
강병욱 <98394884+plain127@users.noreply.github.com>
2025-03-14 15:55:34 +0900

    [Silver I] Title: 미로 탐색, Time: 68 ms, Memory: 34944 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,29 +1,31 @@
 import sys
 from collections import deque
+
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
 graph = [list(map(int, input().strip())) for _ in range(n)]
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
+dx = [-1,0,0,1]
+dy = [0,-1,1,0]
 
-def bfs(x, y):
-    q = deque()
-    q.append([x, y, 1])
-    graph[y][x] = 0
+q = deque([])
+q.append((0,0,1))
+graph[0][0] = 0
+
+result = 0
+
+while q:
+    x, y, count = q.popleft()
     
-    while q :
-        x, y, count = q.popleft()
+    if x == m-1 and y == n-1:
+        result = count
 
-        if x == m-1 and y == n-1:
-            return count
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0<=nx<m and 0<=ny<n and graph[ny][nx] == 1:
-                graph[ny][nx] = 0
-                q.append([nx, ny, count+1])
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
 
-result = bfs(0,0)
+        if 0 <= nx < m and 0 <= ny < n and graph[ny][nx] == 1:
+            q.append((nx, ny, count+1))
+            graph[ny][nx] = 0
 
 print(result)

b6a1814a3e99cafc4e3adc80cad79712f150f583
강병욱 <98394884+plain127@users.noreply.github.com>
2025-03-17 22:45:22 +0900

    [Gold II] Title: 트리의 지름, Time: 464 ms, Memory: 88856 KB -BaekjoonHub

--- 백준/Gold/1167. 트리의 지름/트리의 지름.py
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.py
@@ -1,43 +1,37 @@
 import sys
-from collections import deque
+
+sys.setrecursionlimit(10**9)
 input = sys.stdin.readline
 
 v = int(input())
 
 tree = [[] for _ in range(v+1)]
+visited = [-1]*(v+1)
 
 for _ in range(v):
-    t = list(map(int, input().split()))
-    root = t[0]
-    for i in range(1, len(t)-1):
-        if i % 2 == 0:
-            continue
-        tree[root].append((t[i], t[i+1]))
+    nums = list(map(int, input().split()))
+    root = nums[0]
+
+    i = 1
+    while nums[i] != -1:
+        child = nums[i]
+        weight = nums[i+1]
+        tree[root].append((child, weight))
+        i += 2
+
+def backtracking(node, dist):
+    for child, weight in tree[node]:
+        if visited[child] == -1:
+            visited[child] = dist + weight
+            backtracking(child, dist+weight)
+
+visited[1] = 0
+backtracking(1, 0)
 
 
-def bfs(start, v, tree):
-    far = start
-    max_dia = 0
-    visited = [False]*(v+1)
-    visited[start] = True
-    q = deque()
-    q.append((start, 0))
+leaf = visited.index(max(visited))
+visited = [-1]*(v+1)
+visited[leaf] = 0
+backtracking(leaf, 0)
 
-    while q:
-        s, d = q.popleft()
-        
-        if d > max_dia:
-            max_dia = d
-            far = s
-
-        for n, w in tree[s]:
-            if not visited[n]:
-                visited[n] = True
-                q.append((n, d+w))
-    
-    return far, max_dia
-
-a, _ = bfs(1, v, tree)
-b, diameter = bfs(a, v, tree)
-
-print(diameter)
+print(max(visited))

e7f2a376dbf4cb15cc3a5c8c90e3819d5fb8c78b
강병욱 <98394884+plain127@users.noreply.github.com>
2025-04-06 14:04:23 +0900

    [Silver III] Title: 프린터 큐, Time: 64 ms, Memory: 34944 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.py
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.py
@@ -2,48 +2,35 @@ import sys
 from collections import deque
 
 input = sys.stdin.readline
+results = []
 
-t = int(input())
-counts = []
-
-for _ in range(t):
+for _ in range(int(input())):
     n, m = map(int, input().split())
     q = deque(list(map(int, input().split())))
-
-    count = 0
-    target = q[m]
     idx = m
+    count = 0
 
     while q:
-        if q[0] == target and idx == 0:
-            if max(q) == target:
+        if q[0] < max(q):
+            doc = q.popleft()
+            if idx == 0:
+                q.append(doc)
+                idx = len(q)-1
+            else:
+                q.append(doc)
+                idx -= 1
+        
+        else: 
+            if idx != 0 and q[0] == max(q):
+                q.popleft()
+                idx -= 1
                 count += 1
+
+            elif idx == 0 and q[0] == max(q):
+                count += 1 
                 break
-            else:
-                q.append(q[0])
-                q.popleft()
-                idx = len(q) - 1
-        elif q[0] == target and idx != 0:
-            if q[0] == max(q):
-                q.popleft()
-                count += 1
-                idx -= 1
-            else:
-                q.append(q[0])
-                q.popleft()
-                idx -= 1
 
-        elif q[0] != target:
-            if q[0] == max(q):
-                q.popleft()
-                count += 1
-                idx -= 1
-            else:
-                q.append(q[0])
-                q.popleft()
-                idx -= 1
+    results.append(count)
 
-    counts.append(count)
-
-for count in counts:
-    print(count)
+for result in results:
+    print(result)

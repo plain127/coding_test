2892c94d1745256ce99c95d50af69159fb4f2aed
강병욱 <98394884+plain127@users.noreply.github.com>
2025-08-20 23:32:45 +0900

    [Gold V] Title: Z, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1074. Z/Z.py
+++ 백준/Gold/1074. Z/Z.py
@@ -3,31 +3,21 @@ import sys
 input = sys.stdin.readline
 
 n, r, c = map(int, input().split())
-m = 2**n
-count = 0
 
-def dc(row, col, num):
-    global count
-   
-    if row > r or row + num <= r or col > c or col + num <= c:
-        count += num**2
-        return
-   
-    if num > 2:
-        num//=2
-        dc(row, col, num)
-        dc(row, col+num, num)
-        dc(row+num, col, num)
-        dc(row+num, col+num, num)
+def divide(size, row, col):
+    if size == 0:
+        return 0
+
+    half = 2**(size-1)
+
+    if row < half and col < half:
+        return divide(size-1, row, col)
+    elif row < half and col >= half:
+        return half*half + divide(size-1, row, col-half)
+    elif row >= half and col < half:
+        return 2*half*half + divide(size-1,row-half, col)
     else:
-        if row == r and col == c:
-            print(count)
-        elif row == r and col+1 == c:
-            print(count+1)
-        elif row+1 == r and col == c:
-            print(count+2)
-        else:
-            print(count+3)
-        sys.exit()
+        return 3*half*half + divide(size-1,row-half, col-half)
 
-dc(0,0,m)
+
+print(divide(n, r, c))

3f404c299cb3dbe74a46c206ace062ec3c691f36
강병욱 <98394884+plain127@users.noreply.github.com>
2025-09-15 14:24:03 +0900

    [Silver III] Title: N과 M (3), Time: 1108 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.py
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -3,23 +3,17 @@ import sys
 sys.setrecursionlimit(10**9)
 input = sys.stdin.readline
 
-n, m = map(int,input().split())
-path = []
-visited = [False]*(n+1)
+n, m = map(int, input().split())
+tree = []
 
-def backtracking():
-    if len(path) == m:
-        print(' '.join(map(str, path)))
+def dfs():
+    if len(tree) == m:
+        print(' '.join(map(str, tree)))
         return
 
-    for i in range(1,n+1):
+    for i in range(1, n+1):
+        tree.append(i)
+        dfs()
+        tree.pop()
 
-        visited[i] = True
-        path.append(i)
-
-        backtracking()
-        visited[i] = False
-        path.pop()
-
-
-backtracking()
+dfs()

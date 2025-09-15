99a154dffecfe132fcdfd11cb008fad158e3edd1
강병욱 <98394884+plain127@users.noreply.github.com>
2025-09-15 12:39:46 +0900

    [Silver III] Title: N과 M (1), Time: 96 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,25 +1,11 @@
-import sys 
+import sys
+from itertools import permutations
 
-sys.setrecursionlimit(10**9)
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-graph = []
-visited = [False]*(n+1)
-
-def backtracking():
-    if len(graph) == m:
-        print(' '.join(map(str, graph)))
-        return 
-    
-    for i in range(1,n+1):
-        if visited[i]:
-            continue
-
-        visited[i] = True
-        graph.append(i)
-        backtracking()
-        graph.pop()
-        visited[i] = False
-
-backtracking()
+lst = list(range(1,n+1))
+perm = permutations(lst, m)
+for p in perm:
+    r = map(str, p)
+    print(' '.join(r))

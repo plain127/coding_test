b872769c59231b91ed6781cff637812e4a2358d0
강병욱 <98394884+plain127@users.noreply.github.com>
2026-02-04 21:58:39 +0900

    [Silver I] Title: 곱셈, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -0,0 +1,18 @@
+import sys
+
+input = sys.stdin.readline
+
+a, b, c = map(int, input().split())
+
+def divide(a, b, c):
+    if b == 1:
+        return a%c
+    
+    half = divide(a, b//2, c)
+
+    if b%2 == 0:
+        return (half*half)%c
+    else:
+        return (half*half*a)%c
+
+print(divide(a,b,c))

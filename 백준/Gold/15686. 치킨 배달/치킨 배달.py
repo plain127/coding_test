89364716682322bd8305c405b883330c1d726a7a
강병욱 <98394884+plain127@users.noreply.github.com>
2026-03-27 16:37:38 +0900

    [Gold V] Title: 치킨 배달, Time: 880 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.py
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.py
@@ -5,15 +5,15 @@ input = sys.stdin.readline
 
 n, m = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(n)]
-chicken = []
 
+chicken_house = []
 for row in range(n):
     for col in range(n):
         if graph[row][col] == 2:
-            chicken.append((row, col))
+            chicken_house.append((row,col))
 
 min_dis = int(10**9)
-comb = list(combinations(chicken, m))
+comb = list(combinations(chicken_house, m ))
 
 for c in comb:
     c = list(c)
@@ -21,14 +21,13 @@ for c in comb:
     for row in range(n):
         for col in range(n):
             if graph[row][col] == 1:
-                dis = int(10**9)     
-    
+                dis = int(10**9)
+
                 for i in c:
-                    ro, co = i
+                    ro,co = i
                     dis = min((abs(row-ro) + abs(col-co)), dis)
-                total+=dis
+                total += dis
 
     min_dis = min(min_dis, total)
-
 
 print(min_dis)

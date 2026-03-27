b7366ba620472e36692819ee42a01ae5aad2bd2d
강병욱 <98394884+plain127@users.noreply.github.com>
2026-03-27 14:40:26 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 488 ms, Memory: 66168 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.py
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.py
@@ -1,43 +1,43 @@
-import sys
-import heapq
+import sys, heapq
 
 input = sys.stdin.readline
-INF = int(1e9)
 
 n, e = map(int, input().split())
 
 graph = [[] for _ in range(n+1)]
-
 for _ in range(e):
-    a, b, c =  map(int, input().split())
+    a, b, c = map(int, input().split())
     graph[a].append((b, c))
     graph[b].append((a, c))
 
 v1, v2 = map(int, input().split())
 
+INF = int(1e9)
+
 def dijkstra(start, end):
     distance = [INF]*(n+1)
-    q = []
-    heapq.heappush(q, (0, start))
     distance[start] = 0
+    q = [(0, start)]
 
     while q:
-        dist, now = heapq.heappop(q)
+        weight, node = heapq.heappop(q)
 
-        if distance[now] < dist:
+        if weight > distance[node]:
             continue
-        
-        for next, val  in graph[now]:
-            if distance[next] > distance[now] + val:
-                distance[next] = distance[now] + val
-                heapq.heappush(q,(distance[next], next))
 
+        for nxt, w in graph[node]:
+            sums = weight + w
+            if sums < distance[nxt]:
+                distance[nxt] = sums
+                heapq.heappush(q, (sums, nxt))
+    
     return distance[end]
 
-path1 = dijkstra(1, v1) + dijkstra(v1, v2) + dijkstra(v2, n)
-path2 = dijkstra(1, v2) + dijkstra(v2, v1) + dijkstra(v1, n)
+d1 = dijkstra(1, v1) + dijkstra(v1, v2) + dijkstra(v2, n)
+d2 = dijkstra(1, v2) + dijkstra(v2, v1) + dijkstra(v1, n)
 
-if path1 >= INF and path2 >= INF:
+dis = min(d1, d2)
+if dis >= INF:
     print(-1)
 else:
-    print(min(path1, path2))
+    print(dis)

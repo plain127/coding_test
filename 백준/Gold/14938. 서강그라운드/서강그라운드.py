45e7c00b9d389914be5795977ae1e458fb791e28
강병욱 <98394884+plain127@users.noreply.github.com>
2026-03-27 15:07:43 +0900

    [Gold IV] Title: 서강그라운드, Time: 40 ms, Memory: 35508 KB -BaekjoonHub

--- 백준/Gold/14938. 서강그라운드/서강그라운드.py
+++ 백준/Gold/14938. 서강그라운드/서강그라운드.py
@@ -3,10 +3,8 @@ import sys, heapq
 input = sys.stdin.readline
 
 n, m, r = map(int, input().split())
+t = list(map(int, input().split()))
 graph = [[] for _ in range(n+1)]
-
-weights = list(map(int, input().split()))
-
 for _ in range(r):
     a, b, l = map(int, input().split())
     graph[a].append((b, l))
@@ -16,31 +14,30 @@ INF = int(1e9)
 
 def dijkstra(start):
     distance = [INF]*(n+1)
-
-    q = [(0, start)]
     distance[start] = 0
+    q = [(0, start)]
 
     while q:
-        edge, node = heapq.heappop(q)
-
-        if edge > distance[node]:
+        weight, node = heapq.heappop(q)
+        
+        if weight > distance[node]:
             continue
 
-        for nxt, w in graph[node]:
-            sums = w + edge
+        for nxt, dw in graph[node]:
+            sums = weight + dw 
             if sums < distance[nxt]:
                 distance[nxt] = sums
                 heapq.heappush(q, (sums, nxt))
 
-    prices = 0
+    item = 0
     for i in range(1, n+1):
         if distance[i] <= m:
-            prices += weights[i-1]
+            item += t[i-1] 
+    
+    return item
 
-    return prices
-
-max_price = 0
+item = 0
 for i in range(1, n+1):
-    max_price = max(max_price, dijkstra(i))
+    item = max(dijkstra(i), item)
 
-print(max_price)
+print(item)

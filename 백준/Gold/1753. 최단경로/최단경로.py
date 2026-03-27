19a30bb161a037efcece348fcaa9476365692798
강병욱 <98394884+plain127@users.noreply.github.com>
2026-03-27 14:05:56 +0900

    [Gold IV] Title: 최단경로, Time: 688 ms, Memory: 69300 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -1,40 +1,35 @@
-import sys
-import heapq
+import sys, heapq
 
 input = sys.stdin.readline
-INF = int(1e9)
 
 v, e = map(int, input().split())
 k = int(input())
 
 graph = [[] for _ in range(v+1)]
-distance = [INF]*(v+1)
-
 for _ in range(e):
     u, y, w = map(int, input().split())
-    graph[u].append((y,w))
+    graph[u].append((y, w))
 
-def dijkstra(start):
-    q = []
-    heapq.heappush(q, (0, start))
-    distance[start] = 0
+INF = int(1e9)
+distance = [INF]*(v+1)
 
-    while q:
-        dist, now = heapq.heappop(q)
-        
-        if distance[now] < dist:
-            continue
-        
-        for i in graph[now]:
-            cost = dist + i[1]
-            if cost < distance[i[0]]:
-                distance[i[0]] = cost
-                heapq.heappush(q,(cost,i[0]))
+q = [(0, k)]
+distance[k] = 0
 
-dijkstra(k)
+while q:
+    weight, node = heapq.heappop(q)
 
-for i in range(1, v+1): 
+    if weight > distance[node]:
+        continue
+
+    for nxt, w in graph[node]:
+        sums = weight + w
+        if sums < distance[nxt]:
+            distance[nxt] = sums
+            heapq.heappush(q, (sums, nxt))
+
+for i in range(1,v+1):
     if distance[i] == INF:
-        print('INF')
+        print("INF")
     else:
         print(distance[i])

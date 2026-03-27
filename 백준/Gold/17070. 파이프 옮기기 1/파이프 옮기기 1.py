7ead4ef78bf71a6f3ea196b4748afb977c5eb23c
강병욱 <98394884+plain127@users.noreply.github.com>
2026-03-27 16:08:19 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.py
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.py
@@ -4,6 +4,7 @@ input = sys.stdin.readline
 
 n = int(input())
 graph = [list(map(int, input().split())) for _ in range(n)]
+
 dp = [[[0]*3 for _ in range(n)] for _ in range(n)]
 
 if graph[0][1] == 0:
@@ -18,12 +19,12 @@ for row in range(n):
 
         if col > 0:
             dp[row][col][0] += dp[row][col-1][0] + dp[row][col-1][2]
-
+        
         if row > 0:
             dp[row][col][1] += dp[row-1][col][1] + dp[row-1][col][2]
-
+        
         if row > 0 and col > 0:
             if graph[row][col-1] == 0 and graph[row-1][col] == 0:
-                dp[row][col][2] += dp[row-1][col-1][0] + dp[row-1][col-1][1] + dp[row-1][col-1][2]
+                dp[row][col][2] = dp[row-1][col-1][0] + dp[row-1][col-1][1] + dp[row-1][col-1][2]
 
 print(sum(dp[n-1][n-1]))

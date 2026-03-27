e141d26136dbd34ff91efc17f1420344778a0c0e
강병욱 <98394884+plain127@users.noreply.github.com>
2026-03-27 15:57:18 +0900

    [Gold IV] Title: 거짓말, Time: 40 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Gold/1043. 거짓말/거짓말.py
+++ 백준/Gold/1043. 거짓말/거짓말.py
@@ -3,13 +3,13 @@ import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-line = list(map(int, input().split()))
-truth = set(line[1:])
+t = list(map(int ,input().split()))
+truth = set(t[1:])
 
-parties = []
+party = []
 for _ in range(m):
     data = list(map(int, input().split()))
-    parties.append(data[1:])
+    party.append(data[1:])
 
 parent = list(range(n+1))
 def find(x):
@@ -18,19 +18,21 @@ def find(x):
         x = parent[x]
     return x
 
-def union(a,b):
-    ra, rb = find(a), find(b)
+def union(a, b):
+    ra = find(a)
+    rb = find(b)
+
     if ra != rb:
         parent[rb] = ra
 
-for ppl in parties:
-    for i in range(1, len(ppl)):
-        union(ppl[0], ppl[i])
+for p in party:
+    for i in range(1, len(p)):
+        union(p[0], p[i])
 
 truth_roots = {find(x) for x in truth}
 ans = 0
-for ppl in parties:
-    if all(find(p) not in truth_roots for p in ppl):
+for p in party:
+    if all(find(pp) not in truth_roots for pp in p):
         ans += 1
 
 print(ans)

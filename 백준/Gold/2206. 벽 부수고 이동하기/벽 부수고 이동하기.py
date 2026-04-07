43f84f724f7a882cf949dfc63f8a36104d9e9fc5
강병욱 <98394884+plain127@users.noreply.github.com>
2026-04-07 14:52:04 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 3636 ms, Memory: 191728 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -4,37 +4,32 @@ from collections import deque
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-map = [list(map(int, input().strip())) for _ in range(n)]
-visited = [[[0, 0] for _ in range(m)] for _ in range(n)]
+graph = [list(map(int, input().strip())) for _ in range(n)]
+dirs = [(-1,0),(0,-1),(0,1),(1,0)]
 
-
-dx = [-1,0,0,1]
-dy = [0,-1,1,0]
-
-def bfs(x, y):
-    q = deque([])
-    q.append((x, y, 0))
-    visited[y][x][0] = 1
+def bfs(row, col):
+    visited = [[[0,0] for _ in range(m)] for _ in range(n)]
+    q = deque([(row, col, 0)])
+    visited[row][col][0] = 1
 
     while q:
-        x, y, count = q.popleft()
+        r, c, cnt = q.popleft()
 
-        if (x, y) == (m-1, n-1):
-            return visited[y][x][count]
+        if (r, c) == (n-1, m-1):
+            return visited[r][c][cnt]
 
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
+        for dr, dc in dirs:
+            nr, nc = r+dr, c+dc
 
-            if 0 <= nx < m and 0 <= ny < n:
-                if map[ny][nx] == 1 and count == 0 and visited[ny][nx][1] == 0:
-                    visited[ny][nx][1] = visited[y][x][count] + 1
-                    q.append((nx, ny, 1))
+            if 0<=nr<n and 0<=nc<m:
+                if graph[nr][nc] == 1 and cnt == 0 and visited[nr][nc][1] == 0:
+                    visited[nr][nc][1] = visited[r][c][cnt] + 1
+                    q.append((nr, nc, 1))
                 
-                if map[ny][nx] == 0 and visited[ny][nx][count] == 0:
-                    visited[ny][nx][count] = visited[y][x][count] + 1
-                    q.append((nx, ny, count))
+                if graph[nr][nc] == 0 and visited[nr][nc][cnt] == 0:
+                    visited[nr][nc][cnt] = visited[r][c][cnt] + 1
+                    q.append((nr, nc, cnt))
 
     return -1
-
-print(bfs(0, 0))
+    
+print(bfs(0,0))

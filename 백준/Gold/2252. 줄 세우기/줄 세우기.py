d057161005ea5b8a654781b1caa5d6491858551c
강병욱 <98394884+plain127@users.noreply.github.com>
2026-04-20 19:22:08 +0900

    [Gold III] Title: 줄 세우기, Time: 196 ms, Memory: 40896 KB -BaekjoonHub

---
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.py
@@ -0,0 +1,30 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+graph = [[] for _ in range(n+1)]
+indegree = [0]*(n+1)
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    indegree[b] += 1
+
+q = deque()
+for x in range(1, n+1):
+    if indegree[x] == 0:
+        q.append(x)
+
+result = []
+while q:
+    cur = q.popleft()
+    result.append(cur)
+    for nxt in graph[cur]:
+        indegree[nxt] -= 1
+        if indegree[nxt] == 0:
+            q.append(nxt)
+
+print(*result)

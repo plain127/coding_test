30caf7ca30050c79620e8677f5cc1ebd45222b7f
강병욱 <98394884+plain127@users.noreply.github.com>
2026-04-21 20:00:47 +0900

    [Gold II] Title: 문제집, Time: 192 ms, Memory: 41656 KB -BaekjoonHub

---
+++ 백준/Gold/1766. 문제집/문제집.py
@@ -0,0 +1,28 @@
+import sys, heapq
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+indegree = [0]*(n+1)
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    indegree[b]+=1
+
+hq = []
+for i in range(1,n+1):
+    if indegree[i] == 0:
+        hq.append(i)
+
+ans = []
+while hq:
+    cur = heapq.heappop(hq)
+    ans.append(cur)
+    for nxt in graph[cur]:
+        indegree[nxt] -= 1
+        if indegree[nxt] == 0:
+            heapq.heappush(hq, nxt)
+
+print(*ans)
